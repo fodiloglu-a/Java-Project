820eebb19574c831e3dd2d2511f118b9047e78de
fodiloglu-a <109350249+fodiloglu-a@users.noreply.github.com>
2022-11-10 02:00:15 +0300

    a
    a

---
+++ RentCarSys/ex.java
@@ -0,0 +1,13 @@
+package WeakTwo.RentCarSys;
+
+import java.util.Scanner;
+
+public class ex {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        for (int i=2;i<10;i++){
+            System.out.println("MERHABA "+i);
+        }
+    }
+}
